4fffa62a9fbab0639b2fc0870d92d3a6b6fd7dad
Vishnu P <24x7vishnu@gmail.com>
2024-08-06 11:01:21 +0530

    react bootstrap update

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,14 +1,15 @@
 import React from "react";
+import Card from "react-bootstrap/Card"
 
 function Movies({ title, image, description, industry }) {
   return (
-    <div className="card mb-4 bg-color text-white rounded" id={industry}>
-      <img src={image} className="card-img-top" alt={title} loading="lazy" />
-      <div className="card-body">
-        <h5 className="card-title">{title}</h5>
-        <p className="card-text">{description}</p>
-      </div>
-    </div>
+    <Card className="mb-4 bg-color text-white" id={industry}>
+      <Card.Img variant="top" src={image} alt={title} />
+      <Card.Body>
+        <Card.Title>{title}</Card.Title>
+        <Card.Text>{description}</Card.Text>
+      </Card.Body>
+    </Card>
   );
 }
 
